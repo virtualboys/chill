d5fef2a6c5124789d86124509a177cae207a74f0
david <david@davids-MacBook-Pro.local>
2015-09-26 17:56:56 +0200

    ok

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ io.on('connection', function(socket){
 
 });
 
-var port = server.listen(process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
 var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
  
 http.listen(port, ip_address, function(){
