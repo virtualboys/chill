458002c540ceefd1704aee5a5a7a66ef151864e0
captaincactuz <emanmomot@ufl.edu>
2015-09-30 13:29:10 -0500

    change to port 80

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ io.on('error', function(error) {
     console.log("Error: " + error);
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
 var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
  
 http.listen(port, function(){
